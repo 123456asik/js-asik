ed84f39862d5d23897abc12b31ef98ef70313508
123456asik <154534278+123456asik@users.noreply.github.com>
2024-01-07 16:29:08 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+
+// alert ( 3 + 3 ) // we are using node js, not broser 
+"use strit"; // treat all js code as newer version 
+
+console.log( 3
+    +
+    3) // code readability should be high 
+
+    console.log("asik")
+
+
+    let name = "asik "
+    let age = 18 
+    let isloggedIn = false 
+    let state;
+
+    // number => 2 to power 53
+    // bigint 
+    // string => ""
+    // boolean => true/false 
+    // null => standalone value 
+    // undefined =>
+    // symbol = > unique 
+
+
+    // object 
+
+    console.log( typeof undefined ); // undefined 
+    console.log( typeof null ); // object
